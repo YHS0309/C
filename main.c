f63ca459923ebedac0af45a823595e64c6017931
YHS0309 <165767059+YHS0309@users.noreply.github.com>
2024-09-08 16:05:33 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  c HW.1
+//
+//  Created by YHS on 2024/9/8.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
